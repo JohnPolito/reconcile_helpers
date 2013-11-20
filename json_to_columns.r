e3a22eda6a6ab81bd8b9268ca7aec204a97e40fc
andilab <andi@MacBook-Pro-Andrzej.local>
2013-11-20 16:32:31 +0100

    json to csv for PoS

---
+++ json_to_columns.r
@@ -0,0 +1,25 @@
+require(rjson)
+
+
+parseJson <- function(s) {
+if(is.na(s)){
+	s<-"{'#NA':0}"
+}
+  y <- fromJSON(gsub("'", '"', s))
+  names(y) <- gsub("#", "", names(y))
+
+  as.data.frame(y)
+}
+file_path=file.choose()
+dane_sites_feat<-read.csv(file_path,header=TRUE, sep=";")
+dane_sites_feat$parts_of_speech[dane_sites_feat$parts_of_speech=="None"]<- "{}"
+dane_sites_feat$parts_of_speech[dane_sites_feat$parts_of_speech=="{}"]<- NA
+
+temp<-rbind.fill(lapply(dane_sites_feat$parts_of_speech, parseJson))
+temp$NA. <- NULL
+#head(temp)
+
+
+dane_sites_feat<-data.frame(dane_sites_feat,temp)
+
+write.csv(dane_sites_feat,file=paste("no_json_",basename(file_path),sep=""))
